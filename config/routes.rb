17f36bb866262ed06bf0294f06db10c3f4141553
nisidahiroaki <57944481+nisidahiroaki@users.noreply.github.com>
2020-03-06 02:30:57 +0900

    post edit destroy  実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   
   root "posts#index"
-  resources :posts, only: [:new, :create, :show, :edit]
+  resources :posts, only: [:new, :create, :show, :edit, :update, :destroy]
 end
