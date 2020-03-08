e528f74692775f35a268f4d1b0970787a045261d
nisidahiroaki <57944481+nisidahiroaki@users.noreply.github.com>
2020-03-08 18:32:07 +0900

    カテゴリー機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :content).merge(user_id: current_user.id)
+    params.require(:post).permit(:title, :content, category_ids: []).merge(user_id: current_user.id)
   end
 
 end
